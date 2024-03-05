7fae0b2548a02388f6dedcf7bccf7879906747a6
Mohabo93 <93mohabo@gafe.molndal.se>
2024-03-05 22:00:32 +0100

    edit yml

--- src/test/java/awsProject/awsProject/AwsProjectApplicationTests.java
+++ src/test/java/awsProject/awsProject/AwsProjectApplicationTests.java
@@ -1,4 +1,4 @@
-package awsProject.awsProject;
+/*package awsProject.awsProject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class AwsProjectApplicationTests {
 	void contextLoads() {
 	}
 
-}
+}*/
